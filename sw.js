be89a226ba72cc438b88f0cb35b6972f32154fbb
yecheng <yecheng@youzan.com>
2017-11-29 16:59:20 +0800

    fix

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function (response) {
-            if (response) {
-                return response;
-            } else {
-                if (event.request.url.endsWith('jpg')) {
-                    return event.respondWith('https://cn.vuejs.org/images/logo.png');
-                }
-                return fetch(event.request);
-            }
-        }).catch(function (err) {
-            console.log(err)
-            return event.respondWith('https://cn.vuejs.org/images/logo.png');
-        })
-    );
+    if (/\.jpg/.test(event.request.url)) {
+        event.respondWith(fetch('https://cn.vuejs.org/images/logo.png'))
+    }
 });
